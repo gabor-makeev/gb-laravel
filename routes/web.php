0897b7d3654c3db94e9aa1b8e40d24631d256390
Gabor Makeev <gabor.makejev@gmail.com>
2023-08-24 21:42:55 +0200

    feat: complete task 3; add user welcome page, about page, news page and specific news (post / article) page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welcome/{username}', static function(string $username): string {
+    return "Hello, {$username}";
+});
+
+Route::get('/about', static function(): string {
+    return "Information about the project";
+});
+
+Route::get('/news', static function(): string {
+    return "News";
+});
+
+Route::get('/news/{id}', static function(int $id): string {
+    return "News #{$id}";
+});
