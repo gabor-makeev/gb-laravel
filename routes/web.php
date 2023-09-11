f4b774af9d1b034d525eea14fd1c430fd8317ec3
Gabor Makeev <gabor.makejev@gmail.com>
2023-09-11 12:54:38 +0200

    refactor: group news web routes

--- routes/web.php
+++ routes/web.php
@@ -30,17 +30,18 @@ Route::get('/about', static function(): string {
 Route::get('/categories', [CategoryController::class, 'index'])
     ->name('category.index');
 
-Route::get('/news/{category}', [NewsController::class, 'index'])
-    ->whereIn('category', ['movies', 'tv series', 'music', 'video games', 'off-topic'])
-    ->name('news.index');
-
-Route::get('/news/{category}/{id}', [NewsController::class, 'show'])
-    ->whereIn('category', ['movies', 'tv series', 'music', 'video games', 'off-topic'])
-    ->whereNumber('id')
-    ->name('news.show');
-
-Route::get('/news/create', [NewsController::class, 'create'])
-    ->name('news.create');
+Route::prefix('news')->name('news.')
+    ->group(function () {
+        Route::get('/{category}', [NewsController::class, 'index'])
+            ->whereIn('category', ['movies', 'tv series', 'music', 'video games', 'off-topic'])
+            ->name('index');
+        Route::get('/{category}/{id}', [NewsController::class, 'show'])
+            ->whereIn('category', ['movies', 'tv series', 'music', 'video games', 'off-topic'])
+            ->whereNumber('id')
+            ->name('show');
+        Route::get('/create', [NewsController::class, 'create'])
+            ->name('create');
+});
 
 Route::get('/welcome/{username}', static function(string $username): string {
     return "Hello, {$username}";
